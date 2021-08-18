f5724e2b1ec252a79b048ae97e43a216317121ca
mmaurice <kreexus@yandex.ru>
2021-08-18 18:06:34 +0400

    no message

--- example/configs/Config.php
+++ example/configs/Config.php
@@ -8,6 +8,6 @@ final class Config extends \mmaurice\api\infoflot\configs\DefaultConfig
         # URL
         'url' => 'https://restapi.infoflot.com/',
         # API-ключ
-        'apiKey' => '446e1ad0f5a04797dc30d7abee8458ea887e051f',
+        'apiKey' => '',
     ];
 }
